cf1f487b66b898564a36bf8ec407889124b7ac94
Sandra Zufman <zinkeen@gmail.com>
2020-12-24 10:50:16 +0200

    added add keep

--- apps/Keep/KeepApp.jsx
+++ apps/Keep/KeepApp.jsx
@@ -3,6 +3,7 @@ import { NoteTxt } from "./cmps/NoteTxt.jsx"
 import { NoteTodos } from "./cmps/NoteTodos.jsx"
 import { NoteImg } from "./cmps/NoteImg.jsx"
 import { NoteFilter } from "./cmps/NoteFilter.jsx"
+import { KeepAdd } from "./cmps/KeepAdd.jsx";
 
 const { Link } = ReactRouterDOM;
 
@@ -28,13 +29,17 @@ export class KeepApp extends React.Component {
     getKeepsForDisplay = () => {
         const { keeps, filterBy } = this.state
         if (!filterBy.txt && filterBy.type==='All') return keeps
-        else {
+        else if(filterBy.txt) {
             const filterRegex = new RegExp(filterBy.txt, 'i')
             return keeps.filter(keep => {
                 if (keep.type === 'NoteText') return filterRegex.test(keep.info.txt)
                 if (keep.type === 'NoteTodos') return filterRegex.test(keep.info.label) || ( keep.info.todos.find(todo => filterRegex.test(todo.txt)))
    
             })
+        } else if (filterBy.type!=='All'){
+            const filterRegex = new RegExp(filterBy.type, 'i')
+            return keeps.filter(keep => filterRegex.test(keep.type))
+               
         }
     }
     onRemoveKeep = (keepId) => {
@@ -54,6 +59,7 @@ export class KeepApp extends React.Component {
             <section className="keep-app">
                 <h1>Keeps</h1>
                 <NoteFilter setFilter={this.onSetFilter} />
+                <KeepAdd/>
                 <section className="notes-container">
                     {keeps.map((keep, idx) => <div key={idx}>
                         <DynamicSurveyCmp currCmp={keep.type} keepId={keep.id} info={keep.info} onRemoveKeep={this.onRemoveKeep} />
---
+++ apps/Keep/cmps/KeepAdd.jsx
@@ -0,0 +1,77 @@
+import { keepService } from "../services/keepService.js";
+
+export class KeepAdd extends React.Component {
+
+    state = {
+        keep: {
+            type: 'NoteText',
+            info: {
+                txt: 'Fullstack Me Baby!'
+            }
+        },
+        wellcomeMsg: 'What is on your mind',
+        key: 'txt'
+
+    };
+
+    refInput = React.createRef();
+
+    componentDidMount() {
+
+        this.refInput.current.focus();
+
+        keepService.getTemplateKeep('NoteText').then(({ keep, wellcomeMsg, key }) => {
+            this.setState({ keep, wellcomeMsg, key });
+        });
+    }
+
+    onChooseKeepType = (type) => {
+        
+        keepService.getTemplateKeep(type).then(({ keep, wellcomeMsg, key }) => {
+            this.setState({ keep, wellcomeMsg, key });
+        })
+    }
+
+    onSaveKeep = (ev) => {//on submit
+        ev.preventDefault();
+
+        if (!this.state.keep.info) {
+            return;
+        }
+
+        keepService.save(this.state.keep)
+        .then(savedKeep => {
+            console.log('Saves succesfully', savedKeep );
+            
+        })
+
+    };
+
+    onInputChange = (ev) => {
+        const keep = { ...this.state.keep };
+
+        const value = (keep.type!=='NoteTodos')? ev.target.value:  keepService.turnToToDos(ev.target.value)
+        keep.info[ev.target.name] = value
+        this.setState({
+            keep
+        });
+    };
+
+
+    render() {
+        return (
+            <form onSubmit={this.onSaveKeep}>
+
+                <input value={this.state.keep.info[this.state.key]} ref={this.refInput}
+                    placeholder={this.state.wellcomeMsg} type="text" name={this.state.key}
+                    onChange={this.onInputChange} />
+                <button type='button' onClick={() => this.onChooseKeepType('NoteText')}>Text</button>
+                <button  type='button'onClick={() => this.onChooseKeepType('NoteImg')}>Image</button>
+                <button  type='button' onClick={() => this.onChooseKeepType('NoteTodos')}>List</button>
+                <button type='submit'>Save</button>
+
+
+            </form>
+        );
+    }
+}
